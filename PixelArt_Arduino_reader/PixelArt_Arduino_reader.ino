2da15535b65c2d420d5cecc4e244fd1a34f2f0bb
Nathan <nathan.johnson999@aggiemail.usu.edu>
2019-12-16 15:28:41 -0700

    added folder adn file for reading into arduino to make the art machine work

---
+++ PixelArt_Arduino_reader/PixelArt_Arduino_reader.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600)
+  while{
+    ; //waits for the serial to begin
+  }
+
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
